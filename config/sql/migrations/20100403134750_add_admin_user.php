2be8d0bafd62b096ca5326c6a3f0bb7a99cfd38d
obiwankenobi <obiwankenobi@obiwankenobi-nb>
2010-04-03 14:29:47 -0400

    Adicionando migration de criação do usuário admin

---
+++ config/sql/migrations/20100403134750_add_admin_user.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * AddAdminUser Migration
+ *
+ * @since 04/03/2010 13:47:50
+ */
+class AddAdminUser extends AppMigration {
+
+  var $uses = array('User');
+  
+/**
+ * Up Method
+ *
+ * @return void
+ * @access public
+ */
+	function up() {
+    $user['User'] = array('username' => 'admin',
+                                  'password' => sha1('admin'),
+                                  'email' => 'admin@example.com',
+                                  'name' => 'Admin',
+                                  'nickname' => 'admin',
+                                  'birthday' => '1970-04-07',
+                                  'active' => 1,
+                                  'cpf' => '000.000.000-00',
+                                  'address' => 'R. do Admin',
+                                  'city' => '',
+                                  'state' => '',
+                                  'phone' => '',
+      );
+    
+    if (!$this -> User -> save($user)) {
+      var_dump($this -> User -> validationErrors);
+    }
+	}
+
+/**
+ * Down Method
+ *
+ * @return void
+ * @access public
+ */
+	function down() {
+    $u = $this -> User -> findByUsername('admin');
+    $this -> User -> delete($u['User']['id']);
+	}
+}
+
+?>
