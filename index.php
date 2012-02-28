481ec606134f55d8600c58759a1743fd7418054a
Cauan Cabral <cauan@radig.com.br>
2012-02-27 22:23:57 -0400

    Atualização dos index.php e .htaccess
    Remoção do AppHelper
    Inclusão dos diretórios temporários padrão

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php
 /**
- * PHP versions 5
+ * PHP 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * 
- * Comitiva : Sistema de gerenciamento de eventos ( http://comitiva.phpms.org )
- * Copyright 2010, Grupo de Desenvolvedores PHP de Mato Grosso do Sul - PHPMS ( http://phpms.org )
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @copyright     Copyright 2010, Grupo de Desenvolvedores PHP de Mato Grosso do Sul - PHPMS ( http://phpms.org )
- * @link          http://comitiva.phpms.org Comitiva Project
- * @package       cake
- * @subpackage    cake.comitiva
+ * @package       app
+ * @since         CakePHP(tm) v 0.10.0.1076
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
-?>
