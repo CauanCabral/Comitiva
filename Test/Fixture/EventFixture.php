408dd9fd3d7cec20508a3c6c8cdc017ab2bb700c
michel <michelmottadasilva@gmail.com>
2013-07-23 15:57:22 -0400

    Testes nos métodos dos modelos

--- Test/Fixture/EventFixture.php
+++ Test/Fixture/EventFixture.php
@@ -44,7 +44,7 @@ class EventFixture extends CakeTestFixture {
 		array(
 			'id' => 1,
 			'open' => 1,
-			'title' => 'Lorem ipsum dolor sit amet',
+			'title' => 'evento do michel',
 			'lead' => 'Lorem ipsum dolor sit amet',
 			'alias' => 'Lorem ipsum dolor sit amet',
 			'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
